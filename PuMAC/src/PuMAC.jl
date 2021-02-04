6785a56ae697ea92113c4500a83afbcfe42d2535
JacoboL <43689134+JacoboL@users.noreply.github.com>
2021-02-03 18:36:38 -0600

    Add files via upload

---
+++ PuMAC/src/PuMAC.jl
@@ -0,0 +1,8 @@
+module PuMAC
+
+# Write your package code here.
+include("fuciones.jl")
+
+export con_archivos
+export con_carpeta
+end
---
+++ PuMAC/test/runtests.jl
@@ -0,0 +1,6 @@
+using PuMAC
+using Test
+
+@testset "PuMAC.jl" begin
+    # Write your tests here.
+end
